9cd84d4c160d2c2a8939d762059d51f09e803863
Charlotte <cb1140@canterbury.ac.uk>
2022-01-04 14:47:56 +0000

    created new bootstrap css file

---
+++ main.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+    
+        <!-- Required meta tags -->
+       <meta charset="utf-8">
+       <meta name="viewport" content="width=device-width, inital-scale=1">
+
+    
+       <!-- CSS -->
+       <link rel="stylesheet" href="style.css">
+
+       <!-- jQuery library -->
+       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+       <!-- Latest compiled JavaScript -->
+       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+       <title>Easy Peasy Tracker</title>
+
+    </head>
+
+    <body>
+        
+       <div class="row">
+            <div class="col-sm-4">.col-sm-4</div>
+            <div class="col-sm-4">.col-sm-4</div>
+            <div class="col-sm-4">.col-sm-4</div>
+       </div>
+
+
+
+    </body>
+
+</html>
