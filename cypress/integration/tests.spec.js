42ec6a0a594dcde9444485a48a72b74a28824b5a
Charlotte <72128394+cb1140@users.noreply.github.com>
2022-05-22 17:11:43 +0100

    cypress test

--- cypress/integration/tests.spec.js
+++ cypress/integration/tests.spec.js
@@ -9,5 +9,6 @@
 describe('Loading Test', () => {
     it('Checks that site loads', () => {
       cy.visit('https://5500-cb1140-easypeasytracker-2kt6enmtrdb.ws-eu45.gitpod.io/main_code/main.html')
+	  cy.url().should('include', '/main_code/main.html')
     })
   })
