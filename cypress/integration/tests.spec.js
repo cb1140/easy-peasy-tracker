414f70d1081efa252cb168e436a435c38d3ab377
Charlotte <12brownc2001@gmail.com>
2022-05-22 16:02:36 +0000

    First test

--- cypress/integration/tests.spec.js
+++ cypress/integration/tests.spec.js
@@ -4,3 +4,10 @@
 // If you're unfamiliar with how Cypress works,
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
+
+
+describe('Loading Test', () => {
+    it('Checks that site loads', () => {
+      cy.visit('https://5500-cb1140-easypeasytracker-2kt6enmtrdb.ws-eu45.gitpod.io/main_code/main.html')
+    })
+  })
